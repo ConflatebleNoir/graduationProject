675ca2f62abeed94237c6ebeccaaae950a4a8e0a
Potapenko Max <mpotapenko.mail@gmail.com>
2022-01-24 13:07:23 +0300

    minor corrections (favorites, coworkings slider, header)

--- js/burger.js
+++ js/burger.js
@@ -4,11 +4,20 @@ const menuBurger = document.getElementById('burger');
 const headerMenu = document.getElementById('menu');
 const headerMenuBack = document.getElementById('menu-back');
 
+const body = document.getElementById('body');
+const header = document.getElementById('header');
+const main = document.getElementById('main');
+const footer = document.getElementById('footer');
+
 if (menuBurger) {
     menuBurger.addEventListener("click", function () {
         menuBurger.classList.toggle('active');
         headerMenu.classList.toggle('active');
         headerMenuBack.classList.toggle('active');
+        body.classList.toggle('lock');
+        header.classList.toggle('active');
+        main.classList.toggle('active');
+        footer.classList.toggle('active');
     });
 };
 
@@ -17,5 +26,9 @@ if (headerMenuBack) {
         menuBurger.classList.toggle('active');
         headerMenu.classList.toggle('active');
         headerMenuBack.classList.toggle('active');
+        body.classList.toggle('lock');
+        header.classList.toggle('active');
+        main.classList.toggle('active');
+        footer.classList.toggle('active');
     });
 }
