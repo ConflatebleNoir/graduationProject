89b1dde9927cdd619ba38f37eb93e2527a8036a2
Potapenko Max <mpotapenko.mail@gmail.com>
2022-01-28 16:52:48 +0300

    feedback popup+response

--- js/rate-stars.js
+++ js/rate-stars.js
@@ -6,42 +6,64 @@ const formRateThree = document.getElementById('form-rate-star-third');
 const formRateFour = document.getElementById('form-rate-star-fourth');
 const formRateFive = document.getElementById('form-rate-star-fifth');
 
+const feedbackReset = document.getElementById('feedback-reset');
+
 if (formRateOne) {
     formRateOne.addEventListener("click", function () {
-        formRateOne.classList.toggle('active');
+        formRateOne.classList.add('active');
+        formRateTwo.classList.remove('active');
+        formRateThree.classList.remove('active');
+        formRateFour.classList.remove('active');
+        formRateFive.classList.remove('active');
     });
 };
 
 if (formRateTwo) {
     formRateTwo.addEventListener("click", function () {
-        formRateOne.classList.toggle('active');
-        formRateTwo.classList.toggle('active');
+        formRateOne.classList.add('active');
+        formRateTwo.classList.add('active');
+        formRateThree.classList.remove('active');
+        formRateFour.classList.remove('active');
+        formRateFive.classList.remove('active');
     });
 };
 
 if (formRateThree) {
     formRateThree.addEventListener("click", function () {
-        formRateOne.classList.toggle('active');
-        formRateTwo.classList.toggle('active');
-        formRateThree.classList.toggle('active');
+        formRateOne.classList.add('active');
+        formRateTwo.classList.add('active');
+        formRateThree.classList.add('active');
+        formRateFour.classList.remove('active');
+        formRateFive.classList.remove('active');
     });
 };
 
 if (formRateFour) {
     formRateFour.addEventListener("click", function () {
-        formRateOne.classList.toggle('active');
-        formRateTwo.classList.toggle('active');
-        formRateThree.classList.toggle('active');
-        formRateFour.classList.toggle('active');
+        formRateOne.classList.add('active');
+        formRateTwo.classList.add('active');
+        formRateThree.classList.add('active');
+        formRateFour.classList.add('active');
+        formRateFive.classList.remove('active');
     });
 };
 
 if (formRateFive) {
     formRateFive.addEventListener("click", function () {
-        formRateOne.classList.toggle('active');
-        formRateTwo.classList.toggle('active');
-        formRateThree.classList.toggle('active');
-        formRateFour.classList.toggle('active');
-        formRateFive.classList.toggle('active');
+        formRateOne.classList.add('active');
+        formRateTwo.classList.add('active');
+        formRateThree.classList.add('active');
+        formRateFour.classList.add('active');
+        formRateFive.classList.add('active');
+    });
+};
+
+if (feedbackReset) {
+    feedbackReset.addEventListener("click", function () {
+        formRateOne.classList.remove('active');
+        formRateTwo.classList.remove('active');
+        formRateThree.classList.remove('active');
+        formRateFour.classList.remove('active');
+        formRateFive.classList.remove('active');
     });
 };
