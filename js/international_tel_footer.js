04c170a7f041bc1b2a1a98a0fc0e53fe9ee7cd9d
Potapenko Max <mpotapenko.mail@gmail.com>
2022-02-05 02:48:44 +0300

    default country for telephone inputs

--- js/international_tel_footer.js
+++ js/international_tel_footer.js
@@ -2,16 +2,12 @@
 
 let input = document.querySelector('#phone');
 
-let iti = window.intlTelInput(input, {
+intlTelInput(input, {
     initialCountry: "auto",
-    geoLookup: function (success, failure) {
-        axios({
-            method: 'post',
-            url: 'https://ipinfo.io',
-            responseType: 'json'
-        }).then(function (response) {
-            success(response.data.country);
-        })
+    geoIpLookup: function (success, failure) {
+        $.get("https://ipinfo.io", function () { }, "jsonp").always(function (resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "ru";
+            success(countryCode);
+        });
     },
-    utilsScript: 'js/utils.js'
 });
